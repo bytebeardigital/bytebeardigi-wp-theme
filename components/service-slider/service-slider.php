f7dac751f0bd8ee1f55fffffd7bd9ff804ccfcb8
bytebeardigital <hello@bytebeardigital.com>
2021-11-24 21:55:20 -0500

    service-icon added

--- components/service-slider/service-slider.php
+++ components/service-slider/service-slider.php
@@ -4,14 +4,17 @@ $args = array(
     'post_type' => 'Services'
 );
 $loop = new WP_Query($args);
+
+$service_icon = get_field('service_icon');
 ?>
 
 <div class="slider component container">
        <h2 class="big-heading">What I do</h2></h2>
        <div class=" service__slide">
-        <?php while( $loop->have_posts() ) : $loop->the_post() ; ?>
+        <?php while( $loop->have_posts() ) : $loop->the_post() ;
+         $service_icon = get_field('service_icon'); ?>
         <div class="d-flex service__slide--inner">
-                    <img class="service__slide--img" src="<?php echo get_the_post_thumbnail_url();?>" />
+                    <img class="service__slide--img" src="<?php echo $service_icon;?>" />
                         <div class="col-lg-10 service__slide--content">
 
                             <h4><?php the_title();?></h4>
