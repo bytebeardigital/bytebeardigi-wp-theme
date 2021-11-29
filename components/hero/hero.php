a8814c9db33b90cb853e0640459aabe93c465d47
bytebeardigital <hello@bytebeardigital.com>
2021-11-29 11:28:52 -0500

    hero video fixes

--- components/hero/hero.php
+++ components/hero/hero.php
@@ -7,6 +7,8 @@ if (is_home()) {
     $hero = get_field('hero');
 };
 
+pr($hero['video_file']);
+
 $direction = $hero['direction'];
 $image = $hero['background_img'];
 $subtext = $hero['content'];
@@ -20,29 +22,33 @@ $light_gradient = "linear-gradient(rgba(236, 232, 232, .75), rgba( 236, 232, 232
 $noTheme = $theme == 'noTheme';
 $lightTheme = $theme == 'light';
 $darkTheme = $theme == 'dark';
-if($hero): ?>
-<div 
-    id="hero"
-    class=" hero hero__size--<?php echo $size; ?> hero__theme--<?php echo $theme?>"
-    <?php
-if ($noTheme) {
-    echo 'no theme';
-} elseif ($darkTheme) {
-    echo 'style="background-image:' . $dark_gradient .  ', url(); background-position: center;background-size:cover;"';
-} else {
-    echo 'style="background-image:' . $light_gradient .  ', url(); background-position: center;background-size:cover;"';
-}
-?>
-    >
-    
-    <div class="container hero__direction--<?php echo $direction; ?>">
-    <span class="hero--heading"><?php echo $hero['heading']; ?></span>
-    <h1 class="hero__title"><?php echo $subtext; ?></h1>
+$videoTheme = $theme == 'video';
+if ($hero) : ?>
+<div id="hero" class=" hero hero__size--<?php echo $size; ?> hero__theme--<?php echo $theme ?>" <?php
+                                                                                                    if ($noTheme) {
+                                                                                                        echo 'no theme';
+                                                                                                    } elseif ($darkTheme) {
+                                                                                                        echo 'style="background-image:' . $dark_gradient .  ', url(); background-position: center;background-size:cover;"';
+                                                                                                    } elseif ($videoTheme) {
+                                                                                                        echo 'style="background-size:cover;"';
+                                                                                                    } else {
+                                                                                                        echo 'style="background-image:' . $light_gradient .  ', url(); background-position: center;background-size:cover;"';
+                                                                                                    }
 
-    <?php if(is_home()) {
-        echo '<p class="slogan">Web Design + Development</p>';
-    } ?>
+                                                                                                    ?>>
+    <?php if ($theme === 'video') : ?>
+    <video autoplay muted loop>
+        <source src="<?php echo $hero['video_file']; ?>">
+        </source>
+    </video>
+    <?php endif; ?>
+    <div class="container hero__content hero__direction--<?php echo $direction; ?>">
+        <span class="hero--heading"><?php echo $hero['heading']; ?></span>
+        <?php (!empty($subtext)) ? '<h1 class="hero__title">' . $subtext . '</h1>' : '' ?>
+
+        <?php if (is_home()) {
+                echo '<p class="slogan">Web Design + Development</p>';
+            } ?>
     </div>
 </div>
 <?php endif; ?>
-
