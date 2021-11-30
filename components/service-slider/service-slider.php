bb902e9b5c90b6137fc26592f885db9366007c44
bytebeardigital <hello@bytebeardigital.com>
2021-11-30 00:44:41 -0500

    theme fixes

--- components/service-slider/service-slider.php
+++ components/service-slider/service-slider.php
@@ -8,20 +8,21 @@ $loop = new WP_Query($args);
 $service_icon = get_field('service_icon');
 ?>
 
-<div class="slider component container">
-       <h2 class="big-heading">What I do</h2></h2>
-       <div class=" service__slide">
-        <?php while( $loop->have_posts() ) : $loop->the_post() ;
-         $service_icon = get_field('service_icon'); ?>
+<div class="slider container">
+    <h2 class="big-heading">What I do</h2>
+    </h2>
+    <div class=" service__slide">
+        <?php while ($loop->have_posts()) : $loop->the_post();
+            $service_icon = get_field('service_icon'); ?>
         <div class="d-flex service__slide--inner">
-                    <img class="service__slide--img" src="<?php echo $service_icon;?>" />
-                        <div class="col-lg-10 service__slide--content">
+            <img class="service__slide--img" src="<?php echo $service_icon; ?>" />
+            <div class="col-lg-10 service__slide--content">
 
-                            <h4><?php the_title();?></h4>
-                            <p><?php the_excerpt();?></p>
-                            </p>
-                        </div>
-                </div>
+                <h4><?php the_title(); ?></h4>
+                <p><?php the_excerpt(); ?></p>
+                </p>
+            </div>
+        </div>
         <?php endwhile; ?>
-</div>
+    </div>
 </div>
--- front-page.php
+++ front-page.php
@@ -8,25 +8,27 @@ $test_vid = get_field('test_video', 'options');
 $hero = get_field('hero');
 
 ?>
-<?php echo 
+<?php echo
 
-component('hero');?>
+component('hero'); ?>
 <div class="container hello">
-   <div class="row">
-    <div class="block--inner component" >
-       <p class="hello__content">I'm Bryanna a front-end developer + designer, based out of Detroit, MI. I'm passionate about blending the art of design with the skill of programming to create bright, modern, and engaging websites for companies, creatives, and everyone in-between.</p>
+    <div class="row">
+        <div class="block--inner component">
+            <p class="hello__content">I'm Bryanna a front-end developer + designer, based out of Detroit, MI. I'm
+                passionate about blending the art of design with the skill of programming to create bright, modern, and
+                engaging websites for companies, creatives, and everyone in-between.</p>
 
+        </div>
     </div>
-</div>
 
 </div>
 <?php
+
 component('service-slider');
-component('cta-banner');
 component('portfolio-slider');
+component('cta-banner');
 component('footer-cta');
 get_footer();
 
 
 ?>
-
