51acdd67c4368c13604a0bf03da4b93a1c631d7f
bytebeardigital <hello@bytebeardigital.com>
2021-12-02 20:15:09 -0500

    updated

--- components/hero/hero.php
+++ components/hero/hero.php
@@ -22,7 +22,7 @@ $lightTheme = $theme == 'light';
 $darkTheme = $theme == 'dark';
 $videoTheme = $theme == 'video';
 if ($hero) : ?>
-    <div id="hero" class=" hero hero__size--<?php echo $size; ?> hero__theme--<?php echo $theme ?>" <?php
+<div id="hero" class=" hero hero__size--<?php echo $size; ?> hero__theme--<?php echo $theme ?>" <?php
                                                                                                     if ($noTheme) {
                                                                                                         echo 'no theme';
                                                                                                     } elseif ($darkTheme) {
@@ -34,19 +34,21 @@ if ($hero) : ?>
                                                                                                     }
 
                                                                                                     ?>>
-        <?php if ($theme === 'video') : ?>
-            <video class="hero__theme--video--wrapper" autoplay muted loop>
-                <source src="<?php echo $hero['video_file']; ?>">
-                </source>
-            </video>
+    <?php if ($theme === 'video') : ?>
+    <video class="hero__theme--video--wrapper" autoplay muted loop>
+        <source src="<?php echo $hero['video_file']; ?>">
+        </source>
+    </video>
+    <?php endif; ?>
+    <div class="container hero__content hero__direction--<?php echo $direction; ?>">
+        <span class="hero--heading"><?php echo $hero['heading']; ?></span>
+        <?php if (!empty($subtext)) : ?>
+        <h1 class="hero__title"><?php echo $subtext; ?></h1>
         <?php endif; ?>
-        <div class="container hero__content hero__direction--<?php echo $direction; ?>">
-            <span class="hero--heading"><?php echo $hero['heading']; ?></span>
-            <?php echo (!empty($subtext)) ? '<h1 class="hero__title">' . $subtext . '</h1>' : '' ?>
 
-            <?php if (is_home()) {
+        <?php if (is_home()) {
                 echo '<p class="slogan">Web Design + Development</p>';
             } ?>
-        </div>
     </div>
+</div>
 <?php endif; ?>
--- functions/func-style.php
+++ functions/func-style.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Style functions
  *
@@ -10,7 +11,8 @@
 /**
  * Enqueue theme styles.
  */
-function gulp_wp_theme_styles() {
+function gulp_wp_theme_styles()
+{
 
 	/**
 	 * Set a script handle prefix based on theme name.
@@ -21,15 +23,14 @@ function gulp_wp_theme_styles() {
 	$theme_handle_prefix = 'bytebeardigital';
 
 
-		// // Bootstrap : load css part from cdn
-		wp_enqueue_style('bootstrap-nav', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.css');
+	// // Bootstrap : load css part from cdn
+	wp_enqueue_style('bootstrap-nav', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.css');
 
-		
-	wp_enqueue_style( $theme_handle_prefix . '-styles', get_template_directory_uri() . '/assets/css/' . $theme_handle_prefix .'.css');
+
+	wp_enqueue_style($theme_handle_prefix . '-styles', get_template_directory_uri() . '/assets/css/' . $theme_handle_prefix . '.css');
 
 	wp_enqueue_style('slickjs', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css');
 	// FontAwesome
-	wp_enqueue_script( 'font-awesome', 'https://kit.fontawesome.com/de1f4c5b62.js');
-
+	wp_enqueue_script('font-awesome', 'https://kit.fontawesome.com/de1f4c5b62.js');
 }
-add_action( 'wp_enqueue_scripts', 'gulp_wp_theme_styles' );
+add_action('wp_enqueue_scripts', 'gulp_wp_theme_styles');
