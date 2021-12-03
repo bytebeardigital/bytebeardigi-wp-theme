463b0834cd016b2ccfb211b60976e412143604c1
bytebeardigital <hello@bytebeardigital.com>
2021-12-02 20:18:39 -0500

    Update about.php

--- templates/about.php
+++ templates/about.php
@@ -24,22 +24,11 @@ $lightTheme = $theme == 'light';
 $darkTheme = $theme == 'dark';
 $videoTheme = $theme == 'video';
 if ($hero) : ?>
-<div id="hero" class=" hero hero__size--<?php echo $size; ?> hero__theme--<?php echo $theme ?>" <?php
-                                                                                                    if ($noTheme) {
-                                                                                                        echo 'no theme';
-                                                                                                    } elseif ($darkTheme) {
-                                                                                                        echo 'style="background-image:' . $dark_gradient .  ', url(); background-position: center;background-size:cover;"';
-                                                                                                    } elseif ($videoTheme) {
-                                                                                                        echo 'style="background-size:cover;"';
-                                                                                                    } else {
-                                                                                                        echo 'style="background-image:' . $light_gradient .  ', url(); background-position: center;background-size:cover;"';
-                                                                                                    }
-
-                                                                                                    ?>>
+<div id="hero" class=" hero hero__size--<?php echo $size; ?> >
     <?php if ($theme === 'video') : ?>
-    <video class="hero__theme--video--wrapper" autoplay muted loop>
-        <source src="<?php echo $hero['video_file']; ?>">
-        </source>
+    <video class=" hero__theme--video--wrapper" autoplay muted loop>
+    <source src="<?php echo $hero['video_file']; ?>">
+    </source>
     </video>
     <?php endif; ?>
     <div class="container hero__content hero__direction--<?php echo $direction; ?>">
@@ -48,9 +37,6 @@ if ($hero) : ?>
         <h1 class="hero__title"><?php echo $subtext; ?></h1>
         <?php endif; ?>
 
-        <?php if (is_home()) {
-                echo '<p class="slogan">Web Design + Development</p>';
-            } ?>
     </div>
 </div>
 <?php endif; ?>
