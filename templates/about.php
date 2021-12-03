1229ca427f36d2ccfe3fcaf17aa37c6b19e352ca
bytebeardigital <hello@bytebeardigital.com>
2021-12-02 20:19:22 -0500

    Update about.php

--- templates/about.php
+++ templates/about.php
@@ -9,37 +9,8 @@
 $hero = get_field('hero');
 get_header();
 
-$direction = $hero['direction'];
-$image = $hero['background_img'];
-$subtext = $hero['content'];
-$size = $hero['size'];
-$theme = $hero['theme'];
-
-$dark_gradient = "linear-gradient(rgba(33, 45, 64, .80), rgba(33, 45, 64, .80))";
-$light_gradient = "linear-gradient(rgba(236, 232, 232, .75), rgba( 236, 232, 232, .75))";
-
-//THEMES
-$noTheme = $theme == 'noTheme';
-$lightTheme = $theme == 'light';
-$darkTheme = $theme == 'dark';
-$videoTheme = $theme == 'video';
-if ($hero) : ?>
-<div id="hero" class=" hero hero__size--<?php echo $size; ?> >
-    <?php if ($theme === 'video') : ?>
-    <video class=" hero__theme--video--wrapper" autoplay muted loop>
-    <source src="<?php echo $hero['video_file']; ?>">
-    </source>
-    </video>
-    <?php endif; ?>
-    <div class="container hero__content hero__direction--<?php echo $direction; ?>">
-        <span class="hero--heading"><?php echo $hero['heading']; ?></span>
-        <?php if (!empty($subtext)) : ?>
-        <h1 class="hero__title"><?php echo $subtext; ?></h1>
-        <?php endif; ?>
-
-    </div>
-</div>
-<?php endif; ?>
+component('hero');
+?>
 <div class="about-wrapper " style="margin-top: 75px">
     <div class="container">
         <div class="row">
