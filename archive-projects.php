0017ac538e1ecddb2dcd324521710392d1045d91
bytebeardigital <hello@bytebeardigital.com>
2021-12-02 20:17:21 -0500

    fixed hero on archive projects

--- archive-projects.php
+++ archive-projects.php
@@ -26,33 +26,33 @@ $project_link = get_field('project_link'); ?>
         <div id="filters" class="nav justify-content-center">
             <a class="nav-link all-link" data-filter="*">ALL</a>
             <?php if ($terms) : ?>
-                <?php foreach ($terms as $term) : ?>
-                    <a class="nav-link" data-filter=".<?php echo $term->slug ?>"><?php echo $term->name; ?></a>
-                <?php endforeach; ?>
+            <?php foreach ($terms as $term) : ?>
+            <a class="nav-link" data-filter=".<?php echo $term->slug ?>"><?php echo $term->name; ?></a>
+            <?php endforeach; ?>
             <?php endif; ?>
         </div>
     </div>
     <?php if ($loop) : ?>
-        <div id="projectGrid" class="row">
-            <?php if ($loop->have_posts()) : while ($loop->have_posts()) : $loop->the_post(); ?>
-                    <?php
+    <div id="projectGrid" class="row">
+        <?php if ($loop->have_posts()) : while ($loop->have_posts()) : $loop->the_post(); ?>
+        <?php
                     $termsArray = get_the_terms($post->ID, 'project_categories');
                     $termsString = "";
                     foreach ($termsArray as $term) {
                         $termsString .= $term->slug . ' ';
                     }
                     ?>
-                    <a href="<?php echo $project_link; ?>" class="col-lg-3 sing-project mb-4 <?php echo $termsString; ?>">
-                        <div class="project-single">
-                            <?php echo (has_post_thumbnail()) ? the_post_thumbnail() : '' ?>
-                            <div class="hover-contain">
-                                <h3 class="project-title"><?php the_title(); ?></h3>
-                            </div>
-                        </div>
-                    </a>
-            <?php endwhile;
+        <a href="<?php echo $project_link; ?>" class="col-lg-3 sing-project mb-4 <?php echo $termsString; ?>">
+            <div class="project-single">
+                <?php echo (has_post_thumbnail()) ? the_post_thumbnail() : '' ?>
+                <div class="hover-contain">
+                    <h3 class="project-title"><?php the_title(); ?></h3>
+                </div>
+            </div>
+        </a>
+        <?php endwhile;
             endif; ?>
-        </div>
+    </div>
     <?php endif; ?>
 </div>
 <?php
--- templates/about.php
+++ templates/about.php
@@ -1,28 +1,72 @@
 <?php
-/**
-* Template Name: About Page
-*
 
-*/
+/**
+ * Template Name: About Page
+ *
+
+ */
 //hero
 $hero = get_field('hero');
 get_header();
 
-component('hero');
-?>
-<div class="about-wrapper "  style="margin-top: 75px">
+$direction = $hero['direction'];
+$image = $hero['background_img'];
+$subtext = $hero['content'];
+$size = $hero['size'];
+$theme = $hero['theme'];
+
+$dark_gradient = "linear-gradient(rgba(33, 45, 64, .80), rgba(33, 45, 64, .80))";
+$light_gradient = "linear-gradient(rgba(236, 232, 232, .75), rgba( 236, 232, 232, .75))";
+
+//THEMES
+$noTheme = $theme == 'noTheme';
+$lightTheme = $theme == 'light';
+$darkTheme = $theme == 'dark';
+$videoTheme = $theme == 'video';
+if ($hero) : ?>
+<div id="hero" class=" hero hero__size--<?php echo $size; ?> hero__theme--<?php echo $theme ?>" <?php
+                                                                                                    if ($noTheme) {
+                                                                                                        echo 'no theme';
+                                                                                                    } elseif ($darkTheme) {
+                                                                                                        echo 'style="background-image:' . $dark_gradient .  ', url(); background-position: center;background-size:cover;"';
+                                                                                                    } elseif ($videoTheme) {
+                                                                                                        echo 'style="background-size:cover;"';
+                                                                                                    } else {
+                                                                                                        echo 'style="background-image:' . $light_gradient .  ', url(); background-position: center;background-size:cover;"';
+                                                                                                    }
+
+                                                                                                    ?>>
+    <?php if ($theme === 'video') : ?>
+    <video class="hero__theme--video--wrapper" autoplay muted loop>
+        <source src="<?php echo $hero['video_file']; ?>">
+        </source>
+    </video>
+    <?php endif; ?>
+    <div class="container hero__content hero__direction--<?php echo $direction; ?>">
+        <span class="hero--heading"><?php echo $hero['heading']; ?></span>
+        <?php if (!empty($subtext)) : ?>
+        <h1 class="hero__title"><?php echo $subtext; ?></h1>
+        <?php endif; ?>
+
+        <?php if (is_home()) {
+                echo '<p class="slogan">Web Design + Development</p>';
+            } ?>
+    </div>
+</div>
+<?php endif; ?>
+<div class="about-wrapper " style="margin-top: 75px">
     <div class="container">
         <div class="row">
             <div class="col-lg-3">
                 <h2 class="big-heading">Who I Am</h2>
             </div>
             <div class="col-lg-9">
-                <?php the_content();?>
+                <?php the_content(); ?>
             </div>
         </div>
     </div>
 </div>
-<?php 
+<?php
 component('cta-banner');
 // component('cta-skills');
 get_footer();
