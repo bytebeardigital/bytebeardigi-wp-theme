93e4a85ff0c4f3b040f6ab8d4a1782c0011fab89
bytebeardigital <hello@bytebeardigital.com>
2021-12-02 20:28:05 -0500

    Update social-bar-vert.php

--- components/social-bar-vert/social-bar-vert.php
+++ components/social-bar-vert/social-bar-vert.php
@@ -10,39 +10,39 @@ $linkedin = get_field('linkedin', 'options');
     <div class="socialbar__vert d-flex text-center justify-content-center">
 
         <?php if (!empty($facebook)) : ?>
-        <div class="social-link row">
+        <a href="<?php echo $facebook; ?>" class="social-link row">
             <div class="col-lg-2 icon"><?php echo render_svg('facebook') ?></div>
             <div class="col-10">Facebook</div>
-        </div>
+        </a>
         <?php endif; ?>
 
-        <?php if (!empty($$instagram)) : ?>
-        <div class="social-link row">
+        <?php if (!empty($instagram)) : ?>
+        <a href="<?php echo $instagram; ?>" class="social-link row">
             <div class="col-lg-2 icon"><?php echo render_svg('instagram') ?></div>
             <div class="col-10">Instagram</div>
-        </div>
+        </a>
         <?php endif; ?>
 
         <?php if (!empty($linkedin)) : ?>
-        <div class="social-link row">
+        <a href="<?php echo $linkedin; ?>" class="social-link row">
             <div class="col-lg-2 icon"><?php echo render_svg('linkedin') ?> </div>
             <div class="col-10">LinkedIn</div>
-        </div>
+        </a>
         <?php endif; ?>
 
         <?php if (!empty($github)) : ?>
-        <div class="social-link row">
+        <a href="<?php echo $github; ?>" class="social-link row">
             <div class="col-lg-2 icon"><?php echo render_svg('github') ?></div>
             <div class="col-10">GitHub</div>
-        </div>
+        </a>
         <?php endif; ?>
 
 
         <?php if (!empty($youtube)) : ?>
-        <div class="social-link row">
+        <a href="<?php echo $youtube; ?>" class="social-link row">
             <div class="col-lg-2 icon"><?php echo render_svg('youtube') ?> </div>
             <div class="col-10">YouTube</div>
-        </div>
+        </a>
         <?php endif; ?>
     </div>
 </div>
